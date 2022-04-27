091a31d087c1b7d0d589bd27a1cf1a34c9953d9e
temaxuck <Gorropand@gmail.com>
2022-04-28 00:08:53 +0700

    Latex representation error fixed

--- encyclopedia/views/__init__.py
+++ encyclopedia/views/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, url_for, redirect, flash
 from flask_login import login_user, current_user, logout_user, login_required
-from encyclopedia import db
+from encyclopedia import db, hasher
 from encyclopedia.forms import UploadPyramidForm, ExplicitFormulaForm, GeneratingFunctionForm, LoginForm, SignupForm, UpdateProfileForm
 from encyclopedia.models import User, Pyramid, GeneratingFunction, ExplicitFormula
--- run_app.py
+++ run_app.py
@@ -4,12 +4,33 @@ from encyclopedia.models import User, Pyramid, GeneratingFunction, ExplicitFormu
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="192.168.0.2", port=5000)
+    app.run(debug=True, host="192.168.0.5", port=5000)
 
+def get_pyramid(query, filter_by=1):
+    # Get pyramid right from db
+    # filter_by options:
+    #   0: id
+    #   1: sequence_number
+    #   2: generating_function
+    #   3: explicit_formula
+    #   4: user_id
+    
+    filters = {
+        0: Pyramid.id,
+        1: Pyramid.sequence_number,
+        2: Pyramid.generating_function,
+        3: Pyramid.explicit_formula,
+        4: Pyramid.user_id,
+    }
+    
+    return Pyramid.query.filter(filters[filter_by]==query).first()
+    
+    
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Pyramid': Pyramid, 'GeneratingFunction': GeneratingFunction,
-    'ExplicitFormula': ExplicitFormula, 'relations': relations, 'Variable': Variable, 'Formula': Formula}
+    'ExplicitFormula': ExplicitFormula, 'relations': relations, 'Variable': Variable, 'Formula': Formula,
+    'get_pyramid': get_pyramid}
 
 @app.before_first_request
 def create_tables():
