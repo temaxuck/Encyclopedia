13bd7e875aa4ba685dd5675d19bd8ed9c1125d66
temaxuck <Gorropand@gmail.com>
2022-05-31 10:21:06 +0700

    launch options now loaded from .env

--- config.py
+++ config.py
@@ -1,18 +1,25 @@
 import os
 
+
 class Config(object):
     SECRET_KEY = 'cc530c1746dc2c713ccd28936fc870d6f30106582ed09bef074f66376683ccc8'
+    
     POSTGRES_USERNAME = os.environ.get('POSTGRES_USERNAME')
     POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
     POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
     POSTGRES_PORT = os.environ.get('POSTGRES_PORT')
     SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/encyclopedia'
+    RUN_HOST = os.environ.get('RUN_HOST')
+    RUN_PORT = os.environ.get('RUN_PORT')
+    
     # MSEARCH_BACKEND = 'whoosh'
     # WHOOSH_BASE = 'whoosh'
     MSEARCH_PRIMARY_KEY = 'id'
     MSEARCH_ENABLE = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
     REDIS_URL = "redis://:@localhost:6379/0"
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     # ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     # ELASTIC_USER = os.environ.get("ELASTIC_USER")
     # ELASTIC_PASSWORD = os.environ.get("ELASTIC_PASSWORD")
+    
--- run_app.py
+++ run_app.py
@@ -1,30 +1,15 @@
 from encyclopedia import create_app, db
 from encyclopedia.models import User, Pyramid, GeneratingFunction, ExplicitFormula, relations, Variable, Formula
+from encyclopedia.shell import get_pyramid
+
+from dotenv import load_dotenv
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
-
-def get_pyramid(query, filter_by=1):
-    # Get pyramid right from db
-    # filter_by options:
-    #   0: id
-    #   1: sequence_number
-    #   2: generating_function
-    #   3: explicit_formula
-    #   4: user_id
-    
-    filters = {
-        0: Pyramid.id,
-        1: Pyramid.sequence_number,
-        2: Pyramid.generating_function,
-        3: Pyramid.explicit_formula,
-        4: Pyramid.user_id,
-    }
-    
-    return Pyramid.query.filter(filters[filter_by]==query).first()
-    
+    load_dotenv()
+    app.run(debug=True, host=f"{os.environ.get('RUN_HOST')}", port=os.environ.get('RUN_PORT'))
     
 @app.shell_context_processor
 def make_shell_context():
