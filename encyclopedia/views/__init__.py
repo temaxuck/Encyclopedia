2f883d1e5886a7f2dcd40f4ef597004a71880908
temaxuck <gorropand@gmail.com>
2022-06-07 03:33:05 +0000

    Mail service is temporary disabled

--- encyclopedia/views/__init__.py
+++ encyclopedia/views/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, url_for, redirect, flash, current_app
 from flask_login import login_user, current_user, logout_user, login_required
-from encyclopedia import db, hasher, mail
+from encyclopedia import db, hasher
 from encyclopedia.forms import UploadPyramidForm, ExplicitFormulaForm, GeneratingFunctionForm, LoginForm, SignupForm, UpdateProfileForm, RelationForm
 from encyclopedia.models import User, Pyramid, GeneratingFunction, ExplicitFormula
 from sqlalchemy.orm import Session 
