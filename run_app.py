4b2e5a9d7338c611b59cc7eb9a6919a398c0cab9
temaxuck <gorropand@gmail.com>
2022-06-08 14:34:24 +0000

    Some special functions have been added. Formulas cascade on delete

--- run_app.py
+++ run_app.py
@@ -1,6 +1,6 @@
 from encyclopedia import create_app, db
 from encyclopedia.models import User, Pyramid, GeneratingFunction, ExplicitFormula, relations, Variable, Formula
-from encyclopedia.shell import get_pyramid
+from encyclopedia.shell import get_pyramid, delete_last_ef
 
 from dotenv import load_dotenv
 import os
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 def make_shell_context():
     return {'db': db, 'User': User, 'Pyramid': Pyramid, 'GeneratingFunction': GeneratingFunction,
     'ExplicitFormula': ExplicitFormula, 'relations': relations, 'Variable': Variable, 'Formula': Formula,
-    'get_pyramid': get_pyramid}
+    'get_pyramid': get_pyramid, 'delete_last_ef': delete_last_ef}
 
 @app.before_first_request
 def create_tables():
